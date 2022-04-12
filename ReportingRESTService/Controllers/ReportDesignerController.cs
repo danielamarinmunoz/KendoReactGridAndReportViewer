e89c5b7844c00e1332e10c80d6678f8bf26e4a58
Daniela Marin <daniela.marinmunoz@enersize.com>
2022-04-12 09:47:38 +0200

    first commit

---
+++ ReportingRESTService/Controllers/ReportDesignerController.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp.Net6.ReportingRestServiceCorsDemo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Telerik.Reporting.Services;
+    using Telerik.WebReportDesigner.Services;
+    using Telerik.WebReportDesigner.Services.Controllers;
+    using System.Web.Http.Cors;
+
+    [Route("api/reportdesigner")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ReportDesignerController : ReportDesignerControllerBase
+    {
+        public ReportDesignerController(IReportDesignerServiceConfiguration reportDesignerServiceConfiguration, IReportServiceConfiguration reportServiceConfiguration)
+            : base(reportDesignerServiceConfiguration, reportServiceConfiguration)
+        {
+        }
+    }
+}
---
+++ ReportingRESTService/Controllers/ReportsController.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharp.Net6.ReportingRestServiceCorsDemo.Controllers
+{
+    using System.Net;
+    using System.Net.Mail;
+    using Microsoft.AspNetCore.Mvc;
+    using Telerik.Reporting.Services;
+    using Telerik.Reporting.Services.AspNetCore;
+    using System.Web.Http.Cors;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ReportsController : ReportsControllerBase
+    {
+        public ReportsController(IReportServiceConfiguration reportServiceConfiguration)
+            : base(reportServiceConfiguration)
+        {
+        }
+
+        protected override HttpStatusCode SendMailMessage(MailMessage mailMessage)
+        {
+            throw new System.NotImplementedException("This method should be implemented in order to send mail messages");
+
+            // using (var smtpClient = new SmtpClient("smtp01.mycompany.com", 25))
+            // {
+            //     smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //     smtpClient.EnableSsl = false;               
+            //     smtpClient.Send(mailMessage);
+            // }
+            // return HttpStatusCode.OK;
+        }
+    }
+}
